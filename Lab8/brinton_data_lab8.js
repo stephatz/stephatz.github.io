d3fc82d113fefbac4d1757a027b50ba7077986f8
stephatz <stephania.tello.z@gmail.com>
2024-05-07 17:11:55 -0500

    updating brinton data

--- Lab8/brinton_data_lab8.js
+++ Lab8/brinton_data_lab8.js
@@ -1,8 +1,4 @@
-var brinton = {
-"type": "FeatureCollection",
-"name": "brinton_data_lab8",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
+var brinton = [
 { "type": "Feature", "properties": { "name": "Masonic Opera House: What Cheer, IA", "latitude": 41.401397, "longitude": -92.354629, "time": "1/1/1904" }, "geometry": { "type": "Point", "coordinates": [ -92.354629, 41.401397 ] } },
 { "type": "Feature", "properties": { "name": "Masonic Opera House: What Cheer, IA", "latitude": 41.401397, "longitude": -92.354629, "time": "1/2/1904" }, "geometry": { "type": "Point", "coordinates": [ -92.354629, 41.401397 ] } },
 { "type": "Feature", "properties": { "name": "Montezuma Opera House: Montezuma, IA", "latitude": 41.584878, "longitude": -92.527431, "time": "1/4/1904" }, "geometry": { "type": "Point", "coordinates": [ -92.527431, 41.584878 ] } },
@@ -1287,5 +1283,5 @@ var brinton = {
 { "type": "Feature", "properties": { "name": "Opera House", "latitude": 41.580281, "longitude": -92.374907, "time": "12/16/1908" }, "geometry": { "type": "Point", "coordinates": [ -92.374907, 41.580281 ] } },
 { "type": "Feature", "properties": { "name": "Opera House", "latitude": 41.178903, "longitude": -92.251849, "time": "12/17/1908" }, "geometry": { "type": "Point", "coordinates": [ -92.251849, 41.178903 ] } },
 { "type": "Feature", "properties": { "name": "Opera House", "latitude": 41.198372, "longitude": -92.093089, "time": "12/18/1908" }, "geometry": { "type": "Point", "coordinates": [ -92.093089, 41.198372 ] } }
-]
-}
+];
+
